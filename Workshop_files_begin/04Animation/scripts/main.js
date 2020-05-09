18a1b7632a8b88f6c15a29533ffb4b16cfeb2d1a
cgheydra <cgheydra@gmail.com>
2020-05-09 19:05:36 +0200

    animation with code

--- Workshop_files_begin/04Animation/scripts/main.js
+++ Workshop_files_begin/04Animation/scripts/main.js
@@ -6,9 +6,7 @@ var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
 	myOtherBox.object3D.rotation.x += rotationSpeed;
-	myOtherBox.object3D.rotation.y += rotationSpeed;
-	myOtherBox.object3D.rotation.z += rotationSpeed/2;
-	//console.log(myOtherBox.object3D.rotation);
+	console.log(myOtherBox.object3D.rotation.x);
 }
 
-setInterval(spin, 10);
+setInterval(spin, 16); //equivalent to 60 fps
